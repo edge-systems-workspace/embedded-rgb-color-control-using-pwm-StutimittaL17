9241ad25675f7b079c679867ea34126fa3c5a68b
StutimittaL17 <stuti.mittal_cs.aiml24@gla.ac.in>
2026-02-18 14:56:20 +0530

    Add RGB LED pin definitions and init function docs
    Added pin definitions for RGB LED and initialization function documentation.

--- src/main.cpp
+++ src/main.cpp
@@ -9,3 +9,15 @@
  * @author Stutimittal_17
  * @date 18 Feb 2026
  */
+// Pin definitions for RGB LED
+int red_led = 10;    ///< Red LED connected to digital pin 10
+int green_led = 9;   ///< Green LED connected to digital pin 9
+int blue_led = 11;   ///< Blue LED connected to digital pin 11
+
+
+/**
+ * @brief Initializes LED pins as output.
+ * 
+ * This function runs once when the Arduino starts.
+ * It sets all RGB pins to OUTPUT mode.
+ */
